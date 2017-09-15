a2312a48e10466e9940058df9ec38e5deb2f117a
Marc Lennox <marc.lennox@gmail.com>
2017-09-15 11:30:31 -0400

    Revert

--- api/controllers/SmartThingsController.js
+++ api/controllers/SmartThingsController.js
@@ -2,14 +2,15 @@ module.exports = {
 
   token: (req, res) => {
     var token = req.params.token
-    var hub = sails.hooks.insteon.hub()
-    hub.smartThingsToken = token
-    hub.save(function (err) {
+    Hub.update({}, {smartThingsToken: token}).exec((err, record) => {
       if (err) {
         return res.serverError(err)
+      } else {
+        var hub = sails.hooks.insteon.hub()
+        hub.smartThingsToken = token
+        hub.loadSmartThingsEndpoints(token)
+        return res.json({ hub: record })
       }
-      hub.loadSmartThingsEndpoints(token)
-      return res.json({ hub: hub })
     })
   }
 }
