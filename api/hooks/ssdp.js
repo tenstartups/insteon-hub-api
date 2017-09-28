00d29e37cc76a8d0ecb2914fc910f4b9bd9d58e1
Marc Lennox <marc.lennox@gmail.com>
2017-09-28 09:08:52 -0400

    Renamed hook

--- api/hooks/ssdp.js
+++ api/hooks/ssdp.js
@@ -35,7 +35,7 @@ module.exports = (sails) => {
     },
 
     initialize: (cb) => {
-      sails.after('hook:device_init:loaded', () => {
+      sails.after('hook:device_sync:loaded', () => {
         advertiseIP = process.env.DEVICE_ADVERTISE_IP
         if (advertiseIP === undefined) {
           advertiseIP = require('ip').address()
